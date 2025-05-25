a854014bfcd1ae000a596f83358819ace029f20d
George H. Thomas <georgebushdontcare@gmail.com>
2025-05-25 12:21:59 -0400

    dev

---
+++ controllers/display.js
@@ -0,0 +1,16 @@
+export const indexDisplay = async (req, res) => {
+  res.render("index");
+};
+
+export const adminDisplay = (req, res) => {
+  res.render("admin");
+};
+
+export const display404 = (req, res) => {
+  res.status(404).render("404");
+};
+
+export const display500 = (error, req, res, next) => {
+  console.log(error);
+  res.status(500).render("500");
+};
