13dc08696b7ee74aa5360e2d718f54a8c6a05664
George Thomas <fake@email.org>
2025-05-25 18:19:15 -0400

    my sad attempt with classes

--- public/js/parse/parse-articles.js
+++ public/js/parse/parse-articles.js
@@ -39,14 +39,17 @@ export const buildArticleWrapper = async (inputArray) => {
 export const buildArticleTypeListItem = async () => {
   const articleTypeListItem = document.createElement("li");
   articleTypeListItem.id = "article-type-list-item";
+  articleTypeListItem.className = "form";
 
   const articleTypeLabel = document.createElement("label");
   articleTypeLabel.setAttribute("for", "article-type");
   articleTypeLabel.textContent = "Article Type";
+  // articleTypeLabel.className = "form";
 
   const articleTypeSelect = document.createElement("select");
   articleTypeSelect.name = "article-type";
   articleTypeSelect.id = "article-type";
+  // articleTypeSelect.className = "form";
 
   // Create options for article type select
   const optionArray = [
@@ -79,6 +82,7 @@ export const buildArticleTypeListItem = async () => {
 export const buildArticleHowManyListItem = async () => {
   const articleHowManyListItem = document.createElement("li");
   articleHowManyListItem.id = "article-how-many-list-item";
+  articleHowManyListItem.className = "form";
 
   const articleHowManyLabel = document.createElement("label");
   articleHowManyLabel.setAttribute("for", "article-how-many");
@@ -98,6 +102,7 @@ export const buildArticleHowManyListItem = async () => {
 export const buildArticleSortByListItem = async () => {
   const articleSortByListItem = document.createElement("li");
   articleSortByListItem.id = "article-sort-by-list-item";
+  articleSortByListItem.className = "form";
 
   const articleSortByLabel = document.createElement("label");
   articleSortByLabel.setAttribute("for", "article-sort-by");
@@ -134,7 +139,7 @@ export const parseArticleData = async (inputArray) => {
   if (!inputArray || !inputArray.length) return null;
 
   const articleList = document.createElement("ul");
-  articleList.className = "article-list";
+  articleList.className = "article-list data-return";
 
   let isFirst = true;
   const collapseArray = [];
