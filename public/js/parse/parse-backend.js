ef656d674379dfd4875a126e2fee7d2cb0d0897d
George H. Thomas <georgebushdontcare@gmail.com>
2025-05-26 18:59:25 -0400

    parse dev

--- public/js/parse/parse-backend.js
+++ public/js/parse/parse-backend.js
@@ -1,7 +1,7 @@
 import { sendToBack } from "../util.js";
 import { buildArticleData } from "./parse-articles.js";
-import { buildPicSetData } from "./parse-pics.js";
-import { buildVidPageData } from "./parse-vids.js";
+import { buildPicData } from "./parse-pics.js";
+import { buildVidData } from "./parse-vids.js";
 
 //GET BACKEND DATA
 export const getBackendData = async () => {
@@ -23,14 +23,16 @@ export const buildBackendDislay = async (inputData) => {
   if (!inputData) return null;
   const { articleData, picSetData, vidPageData } = inputData;
 
+  console.log("BACKEND DATA", inputData);
+
   const backendDataWrapper = document.createElement("div");
   backendDataWrapper.id = "backend-data-wrapper";
 
   const articleDataWrapper = await buildArticleData(articleData);
-  const picSetDataWrapper = await buildPicSetData(picSetData);
-  const vidPageDataWrapper = await buildVidPageData(vidPageData);
+  const picDataWrapper = await buildPicData(picSetData);
+  const vidDataWrapper = await buildVidData(vidPageData);
 
-  backendDataWrapper.append(articleDataWrapper, picSetDataWrapper, vidPageDataWrapper);
+  backendDataWrapper.append(articleDataWrapper, picDataWrapper, vidDataWrapper);
 
   return backendDataWrapper;
 };
