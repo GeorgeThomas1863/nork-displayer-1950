12472590e742b020d6bd53d394a25906551ec475
George H. Thomas <georgebushdontcare@gmail.com>
2025-05-29 06:35:27 -0400

    fucking finally found it

--- public/js/parse/parse-pics.js
+++ public/js/parse/parse-pics.js
@@ -132,7 +132,7 @@ export const buildPicData = async (inputArray) => {
 
   const picList = document.createElement("ul");
   picList.id = "pic-data-array-element";
-  // picList.className = "hidden";
+  picList.className = "hidden";
 
   for (let i = 0; i < inputArray.length; i++) {
     const picListItem = await buildPicListItem(inputArray[i]);
