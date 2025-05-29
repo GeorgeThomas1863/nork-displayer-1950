83f6989e7d63ebb91dc3fe973194390eb687007b
George Thomas <fake@email.org>
2025-05-29 17:00:39 -0400

    pick up hte pace

--- public/js/get-data/get-articles.js
+++ public/js/get-data/get-articles.js
@@ -1,18 +1,18 @@
-import CONFIG from "../../../config/config.js";
 import { buildInputParams } from "../util.js";
 
 //GET NEW DATA SECTION
 export const getNewArticleData = async () => {
   //get defaults
-  const { defaultArticleArray } = CONFIG;
+
+  const articleDefaultArray = ["fatboy", 5, "article-newest-to-oldest"];
 
   //get user input
   const inputParams = await buildInputParams();
   const { articleType, articleHowMany, articleSortBy } = inputParams;
   const articleInputArray = [articleType, articleHowMany, articleSortBy];
 
-  for (let i = 0; i < defaultArticleArray.length; i++) {
-    if (defaultArticleArray[i] !== articleInputArray[i]) {
+  for (let i = 0; i < articleDefaultArray.length; i++) {
+    if (articleDefaultArray[i] !== articleInputArray[i]) {
       console.log("ALLAHU AKBAR");
       break; // Exit early once we find a mismatch
     }
