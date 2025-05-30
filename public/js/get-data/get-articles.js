1d36cbc223abd0fc1332fe6ac4d85ebdc64472c1
George H. Thomas <georgebushdontcare@gmail.com>
2025-05-29 20:04:50 -0400

    a bit more dev

--- public/js/get-data/get-articles.js
+++ public/js/get-data/get-articles.js
@@ -9,16 +9,20 @@ export const checkNewArticleData = async () => {
 
   //get user input
   const inputParams = await buildInputParams();
-  console.log("INPUT PARAMS", inputParams);
+  if (!inputParams || !inputParams.articleType || !inputParams.articleHowMany || !inputParams.articleSortBy) return null;
 
+  // instract out article inputs
   const { articleType, articleHowMany, articleSortBy } = inputParams;
   const articleInputArray = [articleType, articleHowMany, articleSortBy];
+
+  //set route
+  inputParams.route = "/get-new-article-data-route";
 
   let newArticleData;
   for (let i = 0; i < articleDefaultArray.length; i++) {
     if (articleDefaultArray[i] !== articleInputArray[i]) {
       //define route, send to back
-      newArticleData = await getNewArticleData(inputParams);
+      newArticleData = await sendToBack(inputParams);
       //break if shit returns
       if (newArticleData) break;
     }
@@ -26,27 +30,23 @@ export const checkNewArticleData = async () => {
 
   if (!newArticleData) return null;
 
-  const articleDataWrapper = await buildArticleData(newArticleData);
+  const newArticleDataWrapper = await buildArticleData(newArticleData);
 
   //get backend data wrapper
   const backendDataWrapper = document.getElementById("backend-data-wrapper");
   const articleArrayElement = document.getElementById("article-array-element");
 
-  //please work
-  backendDataWrapper.replaceChildren(articleDataWrapper, articleArrayElement);
+  console.log("%%%%ARTICLE ARRAY ELEMENT");
+  console.log(articleArrayElement);
 
-  console.log("!!!ARTICLE DATA WRAPPER", articleDataWrapper);
+  //please work
+  backendDataWrapper.replaceChildren(newArticleDataWrapper, articleArrayElement);
+
+  console.log("!!!NEW ARTICLE DATA WRAPPER");
+  console.log(newArticleDataWrapper);
+
+  console.log("BACKEND DATA WRAPPER");
+  console.log(backendDataWrapper);
 
   return true;
-};
-
-export const getNewArticleData = async (inputParams) => {
-  if (!inputParams || !inputParams.articleType || !inputParams.articleHowMany || !inputParams.articleSortBy) return null;
-
-  inputParams.route = "/get-new-article-data-route";
-
-  //add in other checks for whether to get data from backend
-
-  const newArticleData = await sendToBack(inputParams);
-  return newArticleData;
 };
