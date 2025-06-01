6fabe6c9e98c4ed6b3c1e230d3f430610ac4c173
George H. Thomas <georgebushdontcare@gmail.com>
2025-06-01 18:39:41 -0400

    Dev

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,8 @@
 import express from "express";
 
 import { indexDisplay, adminDisplay, display404, display500 } from "../controllers/display.js";
-import { adminSubmitRoute, getBackendDataRoute, getNewDataRoute, getNewArticleDataRoute, getNewPicDataRoute, getNewVidDataRoute } from "../controllers/data-controller.js";
+// import { adminSubmitRoute, getBackendDataRoute, getNewDataRoute, getNewArticleDataRoute, getNewPicDataRoute, getNewVidDataRoute } from "../controllers/data-controller.js";
+import { adminSubmitRoute, getBackendDataRoute, getNewDataRoute } from "../controllers/data-controller.js";
 
 const router = express.Router();
 
@@ -10,12 +11,6 @@ router.post("/admin-submit-route", adminSubmitRoute);
 router.post("/get-backend-data-route", getBackendDataRoute);
 
 router.post("/get-new-data-route", getNewDataRoute);
-
-// router.post("/get-new-article-data-route", getNewArticleDataRoute);
-
-// router.post("/get-new-pic-data-route", getNewPicDataRoute);
-
-// router.post("/get-new-vid-data-route", getNewVidDataRoute);
 
 router.get("/", indexDisplay);
 
