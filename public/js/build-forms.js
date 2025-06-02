1ba794c2c9131ca5817093a696f87347762cf256
George Thomas <fake@email.org>
2025-06-02 19:44:04 -0400

    bug

--- public/js/build-forms.js
+++ public/js/build-forms.js
@@ -1,6 +1,6 @@
 import { buildArticleForm } from "./articles/article-elements.js";
 import { buildPicForm } from "./pics/pic-elements.js";
-import { buildVidForm } from "./vids/vid-form.js";
+import { buildVidForm } from "./vids/vid-elements.js";
 
 export const buildInputForms = async () => {
   const formWrapperElement = document.createElement("div");
