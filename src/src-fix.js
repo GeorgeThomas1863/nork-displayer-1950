f2b5290c2cc9f1a2250cfb637705fa89ec95340f
George Thomas <fake@email.org>
2025-06-07 16:18:36 -0400

    major reorg of files on backend

--- src/src-fix.js
+++ src/src-fix.js
@@ -1,6 +1,5 @@
 import CONFIG from "../config/config.js";
-import { clickIdTriggerMap, expandTriggerMap, inputIdTriggerMap } from "../config/map-display.js";
-import { getPicData, getVidData } from "./src-main.js";
+import { getPicData, getVidData } from "./src-get.js";
 
 //FIX DATA SECTION
 export const fixInputDefaults = async (inputObj) => {
@@ -105,6 +104,10 @@ export const fixDataByType = async (inputArray, dataType) => {
   return results;
 };
 
+//-------------------------
+
+//FIX PIC DATA
+
 //rebuild the picArray
 export const fixPicArray = async (inputObj) => {
   if (!inputObj || !inputObj.picArray || !inputObj.picArray.length) return inputObj;
@@ -124,6 +127,10 @@ export const fixPicArray = async (inputObj) => {
   return returnObj;
 };
 
+//---------------------------
+
+//FIX VID DATA
+
 export const fixVidDataArray = async (inputArray) => {
   const results = [];
   for (let i = 0; i < inputArray.length; i++) {
@@ -133,89 +140,4 @@ export const fixVidDataArray = async (inputArray) => {
   }
 
   return results;
-};
-
-//--------------------------------
-
-//CHECK DATA SECTION
-
-export const checkDataType = async (inputObj) => {
-  if (!inputObj) return null;
-  const { picType, vidType, commandType } = inputObj;
-
-  // console.log("COMMAND TYPE");
-  // console.log(commandType);
-
-  let dataType = "";
-  switch (commandType) {
-    case "click":
-      dataType = await checkClickId(inputObj);
-      break;
-
-    case "expand":
-      dataType = await checkExpandType(inputObj);
-      break;
-
-    case "input":
-      dataType = await checkInputId(inputObj);
-      break;
-  }
-
-  if (!dataType) return null;
-
-  if (dataType === "pics") {
-    switch (picType) {
-      case "pic-alone":
-        return "pics";
-
-      case "pic-sets":
-        return "picSets";
-    }
-  }
-
-  if (dataType === "vids") {
-    switch (vidType) {
-      case "vid-alone":
-        return "vids";
-
-      case "vid-pages":
-        return "vidPages";
-    }
-  }
-
-  return dataType;
-};
-
-export const checkExpandType = async (inputObj) => {
-  if (!inputObj || !inputObj.expandType) return null;
-  const { expandType } = inputObj;
-
-  // console.log("INPUT OBJ");
-  // console.log(inputObj);
-
-  const expandTrigger = expandTriggerMap[expandType];
-  return expandTrigger;
-};
-
-export const checkClickId = async (inputObj) => {
-  if (!inputObj || !inputObj.clickId) return null;
-  const { clickId } = inputObj;
-
-  //loop
-  for (const k in clickIdTriggerMap) {
-    const trigger = clickIdTriggerMap[k];
-    if (trigger.includes(clickId)) {
-      return k;
-    }
-  }
-
-  return null;
-};
-
-export const checkInputId = async (inputObj) => {
-  if (!inputObj || !inputObj.inputId) return null;
-  const { inputId } = inputObj;
-
-  const inputTrigger = inputIdTriggerMap[inputId];
-  return inputTrigger;
 };
---
+++ src/src-get.js
@@ -0,0 +1,138 @@
+import fs from "fs";
+import CONFIG from "../config/config.js";
+import { articleTypeMap } from "../config/map-display.js";
+import dbModel from "../models/db-model.js";
+
+//GET PIC DATA SECTION
+export const getPicData = async (picURL) => {
+  if (!picURL) return null;
+
+  const dataParams = {
+    keyToLookup: "url",
+    itemValue: picURL,
+  };
+
+  try {
+    //GET PIC DATA OBJ FROM DOWNLOAD COLLECTION
+    const picDataObj = await getPicExtraData(dataParams);
+    //FIND PIC SOURCE DATA
+    const picSourceObj = await getPicSourceData(picURL);
+    const returnObj = { ...picDataObj, ...picSourceObj };
+
+    return returnObj;
+  } catch (e) {
+    console.log(e.message + "; PICURL: " + e.url + "; SAVE PATH: " + e.savePath);
+    return null;
+  }
+};
+
+export const getPicExtraData = async (params) => {
+  const { picsDownloaded } = CONFIG;
+  const picDataModel = new dbModel(params, picsDownloaded);
+  const picDataObj = await picDataModel.getUniqueItem();
+
+  if (!picDataObj || !picDataObj.savePath) return null;
+  const { savePath } = picDataObj;
+
+  //THROW ERROR IF PIC NOT DOWNLOADED
+  if (!fs.existsSync(savePath)) {
+    const error = new Error("PIC NOT DOWNLOADED");
+    error.url = picURL;
+    error.savePath = savePath;
+    throw error;
+  }
+
+  return picDataObj;
+};
+
+//FIND WHERE PIC IS FROM
+export const getPicSourceData = async (picURL) => {
+  if (!picURL) return null;
+
+  const { articles, picSetContent, vidPageContent } = CONFIG;
+  let picSource = "";
+  let picDate = "";
+
+  //check articles
+  const articleModel = new dbModel({ keyToLookup: "picArray", itemValue: picURL }, articles);
+  const articleObj = await articleModel.getUniqueItem();
+
+  //extract type
+  if (articleObj) {
+    const { title, articleType, date } = articleObj;
+
+    //MAP ARTICLE TYPE HERE!!!
+    const articleTypeText = articleTypeMap[articleType];
+    picSource = `${articleTypeText} Article, Titled: <i>${title}</i>`;
+    picDate = date;
+  }
+
+  //check pic sets
+  const picSetModel = new dbModel({ keyToLookup: "picArray", itemValue: picURL }, picSetContent);
+  const picSetObj = await picSetModel.getUniqueItem();
+  if (picSetObj) {
+    const { title, date } = picSetObj;
+
+    if (!picSource) {
+      picSource = `Pic Set Titled ${title}`;
+    } else {
+      picSource = picSource + ` and Pic Set Titled <i>${title}</i>`;
+    }
+
+    if (!picDate) {
+      picDate = date;
+    }
+  }
+
+  //check vid pages
+  const vidPageModel = new dbModel({ keyToLookup: "thumbnail", itemValue: picURL }, vidPageContent);
+  const vidPageObj = await vidPageModel.getUniqueItem();
+  if (vidPageObj) {
+    const { title, date } = vidPageObj;
+
+    if (!picSource) {
+      picSource = `Vid Page Titled ${title}`;
+    } else {
+      picSource = picSource + ` and Vid Page Titled <i>${title}</i>`;
+    }
+
+    if (!picDate) {
+      picDate = date;
+    }
+  }
+
+  const returnObj = {
+    picSource: picSource,
+    picDate: picDate,
+  };
+
+  return returnObj;
+};
+
+//---------------------------
+
+//GET VID DATA
+export const getVidData = async (vidURL) => {
+  const { vidsDownloaded } = CONFIG;
+
+  const lookupParams = {
+    keyToLookup: "url",
+    itemValue: vidURL,
+  };
+
+  const vidDataModel = new dbModel(lookupParams, vidsDownloaded);
+  const vidObj = await vidDataModel.getUniqueItem();
+
+  //checks if pic exists, return null if it doesnt
+  if (!vidObj || !vidObj.savePath) return null;
+  const { savePath } = vidObj;
+
+  if (!fs.existsSync(savePath)) {
+    const error = new Error("VID NOT DOWNLOADED");
+    error.url = vidURL;
+    error.savePath = savePath;
+    throw error;
+  }
+
+  return vidObj;
+};
