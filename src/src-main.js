4425e05e350f51ee77a2c817ff3b6f58725b2806
George H. Thomas <georgebushdontcare@gmail.com>
2025-06-08 11:53:33 -0400

    dev

--- src/src-main.js
+++ src/src-main.js
@@ -7,7 +7,7 @@ import { fixDataByType, removeInvalidItems } from "./src-fix.js";
 //gets backend data from db
 export const runGetBackendData = async (inputObj) => {
   if (!inputObj || !inputObj.dataType) return null;
-  const { dataType, isFirstLoad } = inputObj;
+  const { dataType } = inputObj;
   const dataObj = {};
 
   const collection = backendDefaultParams[dataType].collection;
