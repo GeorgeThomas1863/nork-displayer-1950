75eecbf6b3c23734af2094ae722e582a07506c1a
George Thomas <fake@email.org>
2025-06-12 19:04:37 -0400

    dev

--- public/js/admin/admin-return.js
+++ public/js/admin/admin-return.js
@@ -4,10 +4,10 @@ export const buildAdminDefaultDisplay = async (inputObj) => {
   const adminBackendList = document.createElement("ul");
   adminBackendList.classList.add("admin-backend-list");
 
-  const listTitleElement = document.createElement("h2");
-  listTitleElement.innerHTML = "Data Already Scraped";
-  listTitleElement.classList.add("admin-backend-list-title");
-  adminBackendList.append(listTitleElement);
+  const defaultTitleElement = document.createElement("h2");
+  defaultTitleElement.innerHTML = "Data Already Scraped";
+  defaultTitleElement.classList.add("admin-backend-default-title");
+  adminBackendList.append(defaultTitleElement);
 
   const keys = Object.keys(allDataObj);
 
@@ -31,6 +31,38 @@ export const buildAdminDefaultDisplay = async (inputObj) => {
 };
 
 export const buildAdminNewDisplay = async (inputObj) => {
-  console.log("BUILD ADMIN NEW DISPLAY");
-  console.log(inputObj);
+  const { scrapeDataObj } = inputObj;
+
+  const adminNewContainer = document.createElement("div");
+  adminNewContainer.classList.add("admin-new-container");
+
+  //get already scraped data
+  const adminBackendList = await buildAdminDefaultDisplay(inputObj);
+
+  //get new data
+  const newDataList = document.createElement("ul");
+  newDataList.classList.add("admin-new-list");
+
+  const newDataTitleElement = document.createElement("h2");
+  newDataTitleElement.innerHTML = "New Data";
+  newDataTitleElement.classList.add("admin-new-title");
+  newDataList.append(newDataTitleElement);
+
+  const keys = Object.keys(scrapeDataObj);
+
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    const value = scrapeDataObj[key];
+
+    const listItem = document.createElement("li");
+    listItem.classList.add("admin-new-list-item");
+
+    listItem.innerHTML = `${key}: ${value}`;
+
+    newDataList.append(listItem);
+  }
+
+  adminNewContainer.append(adminBackendList, newDataList);
+
+  return adminNewContainer;
 };
