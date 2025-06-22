fde9df9bf83ddea9f1ddc861a603943c68c0a8ee
George Thomas <fake@email.org>
2025-06-22 18:12:34 -0400

    backend dev

--- src/src-fix.js
+++ src/src-fix.js
@@ -1,16 +1,9 @@
-// import CONFIG from "../config/config.js";
-// import dbModel from "../models/db-model.js";
+import fs from "fs";
 import { rePullData } from "./src-main.js";
-// import { getPicData, getVidData } from "./src-get.js";
 
 //REMOVE INVALID ITEMS FROM RETURN
-
 export const removeInvalidItems = async (inputArray, dataType, howMany) => {
   if (!inputArray || !inputArray.length || !dataType) return null;
-
-  // console.log("REMOVE INVALID ITEMS DATA TYPE");
-  // console.log(dataType);
-  // console.log(howMany);
 
   const dataReturnArray = [];
   switch (dataType) {
@@ -30,13 +23,15 @@ export const removeInvalidItems = async (inputArray, dataType, howMany) => {
         const picArrayFixed = [];
         for (let j = 0; j < picArray.length; j++) {
           try {
-            const url = picArray[j];
-            const itemExists = await checkItemExists(url);
+            const picObj = picArray[j];
+            if (!picObj || !picObj.savePath) continue;
+            const { savePath } = picObj;
+            const itemExists = await checkItemExists(savePath);
             if (!itemExists) continue;
 
-            picArrayFixed.push(url);
+            picArrayFixed.push(picObj);
           } catch (e) {
-            console.log(e.message + "; SAVE PATH: " + e.savePath + "; PICURL: " + e.url);
+            console.log(e.message + "; SAVE PATH: " + e.savePath);
             continue;
           }
         }
@@ -52,13 +47,13 @@ export const removeInvalidItems = async (inputArray, dataType, howMany) => {
     case "pics":
       for (let i = 0; i < inputArray.length; i++) {
         try {
-          const dataObj = inputArray[i];
-          if (!dataObj || !dataObj.url) continue;
-          const { url } = dataObj;
-          const itemExists = await checkItemExists(url);
+          const picObj = inputArray[i];
+          if (!picObj || !picObj.savePath) continue;
+          const { savePath } = picObj;
+          const itemExists = await checkItemExists(savePath);
           if (!itemExists) continue;
 
-          dataReturnArray.push(dataObj);
+          dataReturnArray.push(picObj);
           if (dataReturnArray.length === howMany) return dataReturnArray;
         } catch (e) {
           console.log(e.message + "; SAVE PATH: " + e.savePath + "; PICURL: " + e.url);
@@ -71,179 +66,44 @@ export const removeInvalidItems = async (inputArray, dataType, howMany) => {
       for (let i = 0; i < inputArray.length; i++) {
         try {
           const dataObj = inputArray[i];
-          // console.log("VID REMOVE DATA OBJ");
-          // console.log(dataObj);
-          if (!dataObj || !dataObj.url) continue;
-          let url = dataObj.url;
+          //check thumbnail first
+          if (!dataObj || !dataObj.thumbnailData || !dataObj.thumbnailData.savePath) continue;
+          const thumbnailExists = await checkItemExists(dataObj.thumbnailData.savePath);
+          if (!thumbnailExists) continue;
 
-          //if vidPages, use check fucking vidURL
-          if (dataType === "vidPages") url = dataObj.vidURL;
+          //check vid exists
+          if (!dataObj || !dataObj.vidData || !dataObj.vidData.savePath) continue;
+          const { savePath } = dataObj.vidData;
 
-          const itemExists = await checkItemExists(url, "vid");
+          const itemExists = await checkItemExists(savePath);
           if (!itemExists) continue;
 
           dataReturnArray.push(dataObj);
           if (dataReturnArray.length === howMany) return dataReturnArray;
         } catch (e) {
-          console.log(e.message + "; SAVE PATH: " + e.savePath + "; VIDURL: " + e.url);
+          console.log(e.message + "; SAVE PATH: " + e.savePath);
           continue;
         }
       }
       break;
   }
 
-  //half assed answer below, might need to do same for picSets
+  //half assed answer below, might need to redo
   const newDataArray = await rePullData(dataType, howMany);
 
   return newDataArray;
 };
 
 //re-write to use fs
-export const checkItemExists = async (url, type = "pic") => {
-  if (!url) return null;
+export const checkItemExists = async (savePath) => {
+  if (!savePath) return null;
 
-  // let itemExists = "";
-  // switch (type) {
-  //   case "pic":
-  //     const params = {
-  //       keyToLookup: "url",
-  //       itemValue: url,
-  //     };
-  //     //throws error if path doesnt exist
-  //     // itemExists = await getPicExtraData(params);
-  //     break;
+  if (!fs.existsSync(savePath)) {
+    const error = new Error("ITEM NOT DOWNLOADED");
+    error.savePath = savePath;
+    throw error;
+  }
 
-  //   case "vid":
-  //     //throws error if path doesnt exist
-  //     // itemExists = await getVidData(url);
-  //     break;
-  // }
-
-  // //returns null if item not in db, throw error
-  // if (!itemExists) {
-  //   const error = new Error("ITEM NOT IN DB");
-  //   error.url = url;
-  //   error.savePath = type;
-  //   throw error;
-  // }
-
+  //otherwise return true
   return true;
 };
-
-//----------------------------
-
-// //ADD IN PIC SETS AND VID PAGES
-// export const fixDataByType = async (inputArray, dataType) => {
-//   if (!inputArray) return null;
-
-//   const results = [];
-//   for (let i = 0; i < inputArray.length; i++) {
-//     const inputObj = inputArray[i];
-
-//     // console.log("FIX DATA BY TYPE INPUT");
-//     // console.log(inputObj);
-
-//     switch (dataType) {
-//       //single pics
-//       case "pics":
-//         const { url } = inputObj;
-
-//         const picDataObj = await getPicData(url);
-//         if (!picDataObj) continue;
-
-//         const picObj = { ...picDataObj, ...inputObj };
-
-//         results.push(picObj);
-//         break;
-
-//       //picArray
-//       case "articles":
-//       case "picSets":
-//         //rebuild pic array (returns INPUTOBJ if no picArray)
-//         const picArrayObj = await fixPicArray(inputObj);
-//         if (!picArrayObj) continue;
-
-//         results.push(picArrayObj);
-//         break;
-
-//       //pics as thumbnails
-//       case "vidPages":
-//         const vidDataObj = await fixVidPageObj(inputObj);
-//         // console.log("VID DATA OBJ");
-//         // console.log(vidDataObj);
-//         if (!vidDataObj) continue;
-
-//         const vidPageObj = { ...vidDataObj, ...inputObj };
-//         results.push(vidPageObj);
-//         break;
-
-//       //might need to fix thumbnail (prob not)
-//       case "vids":
-//         //check if vid exists
-//         try {
-//           const vidAloneDataObj = await getVidData(inputObj.url);
-//           //   console.log("VID ALONE DATA OBJ");
-//           //   console.log(vidAloneDataObj);
-//           if (!vidAloneDataObj) continue;
-
-//           const vidAloneObj = { ...vidAloneDataObj, ...inputObj };
-//           results.push(vidAloneObj);
-//           break;
-//         } catch (e) {
-//           console.log(e.message + "; SAVE PATH: " + e.savePath + "; VIDURL: " + e.url);
-//           continue;
-//         }
-//     }
-//   }
-
-//   return results;
-// };
-
-// //-------------------------
-
-// //FIX PIC DATA
-
-// //rebuild the picArray
-// export const fixPicArray = async (inputObj) => {
-//   if (!inputObj || !inputObj.picArray || !inputObj.picArray.length) return inputObj;
-//   const { picArray } = inputObj;
-//   const returnObj = { ...inputObj };
-
-//   const picDataArray = [];
-//   for (let i = 0; i < picArray.length; i++) {
-//     //gets pic data AND source / date
-//     const picDataObj = await getPicData(picArray[i]);
-//     if (!picDataObj) continue;
-
-//     picDataArray.push(picDataObj);
-//   }
-
-//   returnObj.picArray = picDataArray;
-//   return returnObj;
-// };
-
-// //---------------------------
-
-// //FIX VID DATA
-
-// export const fixVidPageObj = async (inputObj) => {
-//   // console.log("ON THIS PART FAGGOT");
-//   // console.log(inputObj);S
-
-//   if (!inputObj || !inputObj.vidURL) return null;
-//   const { vidURL } = inputObj;
-
-//   //DONT RETURN NULL HERE
-//   try {
-//     const vidDataObj = await getVidData(vidURL);
-//     // if (!vidDataObj) return null;
-
-//     const vidPageObj = { ...vidDataObj, ...inputObj };
-//     return vidPageObj;
-//   } catch (e) {
-//     console.log(e.message + "; SAVE PATH: " + e.savePath + "; VIDURL: " + e.url);
-//     return null;
-//   }
-// };
-
-//--------------------
