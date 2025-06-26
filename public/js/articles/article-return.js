ddac8acaef2cf95a82ec893797ef0e4085d8779a
George Thomas <fake@email.org>
2025-06-26 15:00:50 -0400

    still

--- public/js/articles/article-return.js
+++ public/js/articles/article-return.js
@@ -33,7 +33,7 @@ export const buildArticleListItem = async (inputObj, isFirst) => {
   const { title, date } = inputObj;
 
   const articleListItem = document.createElement("li");
-  articleListItem.className = "article-list-item";
+  articleListItem.className = "article-list-item wrapper";
 
   // Create the article element (now includes pictures inside)
   const articleElement = await buildArticleElement(inputObj);
