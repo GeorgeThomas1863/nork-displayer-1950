a1007e55e5852073ea51ef38341bc0803fac03de
George Thomas <fake@email.org>
2025-06-26 14:11:07 -0400

    backend get vid data still wrong

--- src/src-fix.js
+++ src/src-fix.js
@@ -6,9 +6,9 @@ import dbModel from "../models/db-model.js";
 export const removeInvalidItems = async (inputArray, dataType, howMany) => {
   if (!inputArray || !inputArray.length || !dataType) return null;
 
-  // console.log("REMOVE INVALID ITEMS DATA TYPE");
-  // console.log(dataType);
-  // console.log(inputArray);
+  console.log("REMOVE INVALID ITEMS DATA TYPE");
+  console.log(dataType);
+  console.log(inputArray);
 
   const dataReturnArray = [];
   switch (dataType) {
