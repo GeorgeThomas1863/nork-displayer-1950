a21ad3fbdf29c1bc9e21493821af030814f81aa4
George Thomas <fake@email.org>
2025-06-26 15:50:37 -0400

    collapse dev

--- public/js/collapse.js
+++ public/js/collapse.js
@@ -1,12 +1,12 @@
-import { state } from "./state.js";
+// import { state } from "./state.js";
 
 export const buildCollapseContainer = async (inputObj) => {
   if (!inputObj || !inputObj.titleElement || !inputObj.contentElement) return null;
   const { titleElement, contentElement, isExpanded = false, className = "", dataAttribute = "" } = inputObj;
   // const { isFirstLoad } = state;
 
-  //if not the first load all NOT expanded
-  if (!state.isFirstLoad) isExpanded = false;
+  // //if not the first load all NOT expanded
+  // if (!state.isFirstLoad) isExpanded = false;
 
   // Create container
   const collapseContainer = document.createElement("div");
