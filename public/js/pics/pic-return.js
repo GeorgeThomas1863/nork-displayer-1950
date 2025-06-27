87e5ae9832df4946cd5b8cfddd4a33d6f28fa840
George H. Thomas <georgebushdontcare@gmail.com>
2025-06-26 21:02:05 -0400

    found problem

--- public/js/pics/pic-return.js
+++ public/js/pics/pic-return.js
@@ -62,7 +62,7 @@ export const buildPicContainer = async (inputObj) => {
   picContainerElement.id = "pic-container-element";
 
   //Add pics as collapse
-  const picContainerData = await picDropDownContainer(picArray, "set");
+  const picContainerData = await picDropDownContainer(picArray, "total");
   if (picContainerData) {
     picContainerElement.append(picContainerData);
   }
