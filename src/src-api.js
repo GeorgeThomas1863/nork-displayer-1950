10ec4a7e1fb3b2d31715e974fa19c983a92664c6
George H. Thomas <georgebushdontcare@gmail.com>
2025-07-01 21:18:48 -0400

    Revert "attempt to implement streaming shit"
    This reverts commit 4024c0f52ccd4fbe1e6d8c5eb1a54909fb4d39c8.

--- src/src-api.js
+++ src/src-api.js
@@ -1,35 +1,15 @@
 import axios from "axios";
 import CONFIG from "../config/config.js";
 
-// //SENDS THE ADMIN COMMAND TO THE API
-// export const sendAdminCommand = async (inputParams) => {
-//   const { apiURL } = CONFIG;
-//   try {
-//     const res = await axios.post(apiURL, inputParams);
-
-//     return res.data;
-//   } catch (e) {
-//     console.log(e);
-//     return null;
-//   }
-// };
-
 //SENDS THE ADMIN COMMAND TO THE API
-export const sendAdminCommand = async (inputParams, onUpdate) => {
-  const { apiStartURL, apiStreamURL } = CONFIG;
+export const sendAdminCommand = async (inputParams) => {
+  const { apiURL } = CONFIG;
   try {
-    await axios.post(apiStartURL, inputParams);
+    const res = await axios.post(apiURL, inputParams);
 
-    const eventSource = new EventSource(apiStreamURL);
-
-    eventSource.onmessage = (e) => {
-      const data = JSON.parse(e.data);
-      onUpdate(data);
-    };
-
-    return { eventSource };
-  } catch (error) {
-    console.log(error);
+    return res.data;
+  } catch (e) {
+    console.log(e);
     return null;
   }
 };
