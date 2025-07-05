ad3aac8e82be6e37547f752a1ddaffcddef741b0
George H. Thomas <georgebushdontcare@gmail.com>
2025-07-05 12:19:16 -0400

    dev

--- public/js/articles/article-change.js
+++ public/js/articles/article-change.js
@@ -1,6 +1,8 @@
 import { state } from "../util/state.js";
 
 export const changeArticleType = async (articleType) => {
+  if (!articleType || articleType === state.articleType) return null;
+
   console.log("ARTICLE TYPE");
   console.log(articleType);
 };
