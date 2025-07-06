4477b8a964d81ed935a04efbd077f4376c9dc476
George H. Thomas <georgebushdontcare@gmail.com>
2025-07-05 22:01:45 -0400

    changing article type active state

--- public/js/articles/article-change.js
+++ public/js/articles/article-change.js
@@ -8,6 +8,23 @@ export const changeArticleType = async (articleType) => {
   const updateArticleState = await updateStateEventTriggered(articleType, "article-type");
   if (!updateArticleState) return null;
 
+  //update active article
+  const articleButtonArray = document.querySelectorAll(".article-type-button");
+
+  //remove active class from all buttons
+  for (let i = 0; i < articleButtonArray.length; i++) {
+    const articleButton = articleButtonArray[i];
+    if (!articleButton.classList.contains("active")) continue;
+    articleButton.classList.remove("active");
+  }
+
+  //add active class to the new button
+  const newArticleButton = document.querySelector(`#article-type-button-list [value="${articleType}"]`);
+  
+  console.log("NEW ARTICLE BUTTON");
+  console.log(newArticleButton);
+  if (newArticleButton) newArticleButton.classList.add("active");
+
   //re-run build display
   await buildDisplay();
 
