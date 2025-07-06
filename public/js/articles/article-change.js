a7bb5322c75b54b0f73bfe22748dc335c3042107
George H. Thomas <georgebushdontcare@gmail.com>
2025-07-05 22:21:10 -0400

    dev

--- public/js/articles/article-change.js
+++ public/js/articles/article-change.js
@@ -9,21 +9,21 @@ export const changeArticleType = async (articleType) => {
   if (!updateArticleState) return null;
 
   //update active article
-  const articleButtonArray = document.querySelectorAll(".article-type-button");
+  // const articleButtonArray = document.querySelectorAll(".article-type-button");
 
-  //remove active class from all buttons
-  for (let i = 0; i < articleButtonArray.length; i++) {
-    const articleButton = articleButtonArray[i];
-    if (!articleButton.classList.contains("active")) continue;
-    articleButton.classList.remove("active");
-  }
+  // //remove active class from all buttons
+  // for (let i = 0; i < articleButtonArray.length; i++) {
+  //   const articleButton = articleButtonArray[i];
+  //   if (!articleButton.classList.contains("active")) continue;
+  //   articleButton.classList.remove("active");
+  // }
 
-  // add active class to the new button
-  const newArticleButton = document.querySelector(`[data-article-type="${articleType}"]`);
+  // // add active class to the new button
+  // const newArticleButton = document.querySelector(`[data-article-type="${articleType}"]`);
 
-  // console.log("NEW ARTICLE BUTTON");
-  // console.log(newArticleButton);
-  if (newArticleButton) newArticleButton.classList.add("active");
+  // // console.log("NEW ARTICLE BUTTON");
+  // // console.log(newArticleButton);
+  // if (newArticleButton) newArticleButton.classList.add("active");
 
   //re-run build display
   await buildDisplay();
