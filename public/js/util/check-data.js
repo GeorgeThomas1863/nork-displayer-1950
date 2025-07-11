15295603fbfe76f55fdf71ecaba5d36f8850f6f3
George H. Thomas <georgebushdontcare@gmail.com>
2025-07-11 19:26:16 -0400

    dev

--- public/js/util/check-data.js
+++ public/js/util/check-data.js
@@ -41,9 +41,9 @@ export const checkInputTriggered = async (inputId) => {
 export const checkNewDataNeeded = async () => {
   const { isFirstLoad, dataReq, dataLoaded, dataType, trigger } = state;
 
-  // console.log("AHHHHHHHHHHHHHHHHHHHHHHHHHHH");
-  // console.log("STATE");
-  // console.dir(state);
+  console.log("AHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+  console.log("STATE");
+  console.dir(state);
 
   if (isFirstLoad) return true;
 
