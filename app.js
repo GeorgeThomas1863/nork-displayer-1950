4069c4c559ed556464584c02755b1376e3ca7bba
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-10-09 19:36:19 -0400

    dev

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ import CONFIG from "./config/config.js";
 import express from "express";
 import cors from "cors";
 import routes from "./routes/router.js";
- 
+
+const { expressPicPath, expressVidPath, expressWatchPath, expressConfigPublicPath, picPath, vidPath, watchPath, scrapePort, displayPort } = CONFIG;
+
 const app = express();
 
 //custom paths to expose to frontend
-app.use(CONFIG.expressPicPath, express.static(CONFIG.picPath));
-app.use(CONFIG.expressVidPath, express.static(CONFIG.vidPath));
-app.use(CONFIG.expressWatchPath, express.static(CONFIG.watchPath));
-app.use(CONFIG.expressConfigPublicPath, express.static("config/config-public.js"));
+app.use(expressPicPath, express.static(picPath));
+app.use(expressVidPath, express.static(vidPath));
+app.use(expressWatchPath, express.static(watchPath));
+app.use(expressConfigPublicPath, express.static("config/config-public.js"));
 
 //standard public path
 app.use(express.static("public"));
@@ -21,7 +23,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [`http://localhost:${CONFIG.scrapePort}`],
+    origin: [`http://localhost:${scrapePort}`],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
@@ -31,4 +33,4 @@ app.use(
 app.use(routes);
 
 // app.listen(1801);
-app.listen(CONFIG.displayPort);
+app.listen(displayPort);
