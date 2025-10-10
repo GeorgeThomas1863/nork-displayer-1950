6259d458522a97c95e1fe672673f272ee354917c
GeorgeThomas1863 <133590615+GeorgeThomas1863@users.noreply.github.com>
2025-10-10 19:06:25 -0400

    dev

--- src/state.js
+++ src/state.js
@@ -3,12 +3,13 @@ const state = {
   scrapeActive: false,
   schedulerActive: false,
   dataStep: "null",
-  updateItems: "null",
+  //updateItems: "null",
   scrapeStartTime: null,
   scrapeEndTime: null,
   scrapeLengthSeconds: null,
   scrapeLengthMinutes: "null",
-  scrapeStatusMessage: "null",
+  scrapeStatus: "null",
+  scrapeError: "null",
   scrapeMessage: "null",
 };
 
