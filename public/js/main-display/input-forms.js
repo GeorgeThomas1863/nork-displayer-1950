4d6e98bcf00c4c5e707487eb2ccb68d8a8e14e32
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-10-13 19:06:25 -0400

    dev

--- public/js/main-display/input-forms.js
+++ public/js/main-display/input-forms.js
@@ -1,6 +1,6 @@
-import { buildArticleForm } from "../articles/articles-form.js";
-import { buildPicForm } from "../pics/pics-form.js";
-import { buildVidForm } from "../vids/vids-form.js";
+import { buildArticlesForm } from "../articles/articles-form.js";
+import { buildPicsForm } from "../pics/pics-form.js";
+import { buildVidsForm } from "../vids/vids-form.js";
 // import { buildWatchForm } from "../watch/watch-form.js";
 import { defineCollapseItems } from "../util/collapse-display.js";
 
@@ -8,13 +8,13 @@ export const buildInputForms = async () => {
   const inputFormWrapper = document.createElement("div");
   inputFormWrapper.id = "input-form-wrapper";
 
-  const articleFormWrapper = await buildArticleForm();
+  const articleFormWrapper = await buildArticlesForm();
 
-  const picFormWrapper = await buildPicForm();
+  const picFormWrapper = await buildPicsForm();
 
   // const watchFormWrapper = await buildWatchForm();
 
-  const vidFormWrapper = await buildVidForm();
+  const vidFormWrapper = await buildVidsForm();
 
   //try adding as collapse "group"
   // await defineCollapseItems([articleFormWrapper, picFormWrapper, vidFormWrapper, watchFormWrapper]);
