48d85b561cef241939ab8cb7e6e42cca4e065c39
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-10-22 09:38:23 -0400

    pw hash working

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,13 +1,17 @@
+import bcrypt from "bcrypt";
+
 import CONFIG from "../config/config.js";
 
-export const authController = (req, res) => {
+export const authController = async (req, res) => {
   if (!req.body || !req.body.pw) {
     res.json({ success: false, redirect: "/401" });
     return;
   }
 
+  const pwValid = await bcrypt.compare(req.body.pw, CONFIG.pw);
+
   //pw check
-  if (req.body.pw !== CONFIG.pw) {
+  if (!pwValid) {
     res.json({ success: false, redirect: "/401" });
     return;
   }
@@ -17,14 +21,16 @@ export const authController = (req, res) => {
   res.json({ success: true, redirect: "/" });
 };
 
-export const adminAuthController = (req, res) => {
+export const adminAuthController = async (req, res) => {
   if (!req.body || !req.body.pwAdmin) {
     res.json({ success: false, redirect: "/401" });
     return;
   }
 
+  const pwAdminValid = await bcrypt.compare(req.body.pwAdmin, CONFIG.pwAdmin);
+
   //pw check
-  if (req.body.pwAdmin !== CONFIG.pwAdmin) {
+  if (!pwAdminValid) {
     res.json({ success: false, redirect: "/401" });
     return;
   }
