ddd363a8f1b4f84297de942476753ffcf96028bd
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-10-23 10:15:06 -0400

    admin auth

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,13 @@ export const requireAuth = (req, res, next) => {
 };
 
 export const requireAdminAuth = (req, res, next) => {
+  //first auth check
+  if (!req.session.authenticated) {
+    res.sendFile(path.join(process.cwd(), "html", "auth.html"));
+    return;
+  }
+
+  //then admin auth check
   if (req.session.adminAuthenticated) {
     next();
   } else {
